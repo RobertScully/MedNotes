5184f59b28b7c83471b08452efa61a9cf7a37c5b
RobertScully <c12405568@mydit.ie>
2018-09-02 00:14:41 +0100

    update

--- app.py
+++ app.py
@@ -247,19 +247,6 @@ def insertNote():
     except Exception as e:
         return render_template('error.html',error = str(e))
       
-        
-
-app.config['UPLOAD_FOLDER'] = 'static/Uploads'
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        file = request.file['file']
-        extension = os.path.splitext(file.filename)[1]
-        f_name = str(uuid.uuid4()) + extension
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
-        return json.dumps({'filename':f_name})
 
 
 @app.route('/tesseract')#Fix
